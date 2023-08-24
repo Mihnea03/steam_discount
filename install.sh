38a795506f4650a4852a5377ad1caf837d2e6985
Mihnea03 <stef1eds@gmail.com>
2023-08-24 12:24:07 +0300

    Installation script

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ python3 -m venv env
 echo "Creating environment..."
 source env/bin/activate
 echo "Installing packages..."
-python3 -m pip -U -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Done!"
